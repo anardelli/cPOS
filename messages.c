b8adda523d6aeccfb022edccf637748cdaa4387e
brimonk <bflatmaster@gmail.com>
2017-02-16 08:40:30 -0500

    added basic message functionality

--- messages.c
+++ messages.c
@@ -1,76 +1,74 @@
+#include <string.h>
+#include <stdlib.h>
+#include "messages.h"
+
+
 /*
- * messages.c
- *
- * this message passing interface is described in here:
- *
- *		http://jpos.org/doc/proguide-draft.pdf
- *	
- * messages are essentially a bit mapping affair. So, using the defines
- * below, a simple '|' operation can easily set a map.
+ * func : set
+ * args : struct ISOmsg *, int, char *, int
+ * out  : int
+ * use  : set a message within a given ISOmsg
  */
 
-// remove me
-#include <stdio.h>
-
-
-// Message Type Indicator Bit Fields
-enum HEADER_MESSAGE_MTI {
-HEADER_MTI_VERSION_1987              = 0,
-HEADER_MTI_VERSION_1993              = 1,
-HEADER_MTI_VERSION_2003              = 2,
-
-HEADER_MTI_VERSION_RESERVED_ISO_LOW  = 3,
-HEADER_MTI_VERSION_RESERVED_ISO_HIGH = 7,
-
-HEADER_MTI_VERSION_NATIONAL          = 8,
-HEADER_MTI_VERSION_PRIVATE           = 9
-};
-
-
-
-// Message Class
-enum HEADER_MESSAGE {
-HEADER_MESSAGE_CLASS_RESERVED_0      = 0,
-HEADER_MESSAGE_CLASS_AUTHORIZATION   = 1,
-HEADER_MESSAGE_CLASS_FINANCIAL       = 2,
-HEADER_MESSAGE_CLASS_FILE_UPDATE     = 3,
-HEADER_MESSAGE_CLASS_CHRGBAK         = 4,
-HEADER_MESSAGE_CLASS_RECONCILIATION  = 5,
-HEADER_MESSAGE_CLASS_ADMINISTRATIVE  = 6,
-HEADER_MESSAGE_CLASS_FEECOLLECT      = 7,
-HEADER_MESSAGE_CLASS_NETMANAGE       = 8,
-HEADER_MESSAGE_CLASS_RESERVED_9      = 9
-};
-
-
-// Message Function
-enum HEADER_MESSAGE_FUNC {
-HEADER_MESSAGE_FUNCTION_REQUEST           = 0,
-HEADER_MESSAGE_FUNCTION_REQUEST_RESPONSE  = 1,
-HEADER_MESSAGE_FUNCTION_ADVICE            = 2,
-HEADER_MESSAGE_FUNCTION_ADVICE_RESPONSE   = 3,
-HEADER_MESSAGE_FUNCTION_NOTIFICATION      = 4,
-
-HEADER_MESSAGE_FUNCTION_RESERVED_ISO_LOW  = 5,
-HEADER_MESSAGE_FUNCTION_RESERVED_ISO_HIGH = 9
-};
-
-// Transaction Originator
-enum HEADER_TRANS_ORIGINATOR {
-HEADER_TRANS_ORIGINATOR_ACQUIRER           = 0,
-HEADER_TRANS_ORIGINATOR_ACQUIRER_REPEAT    = 1,
-HEADER_TRANS_ORIGINATOR_CARD_ISSUER        = 2,
-HEADER_TRANS_ORIGINATOR_CARD_ISSUER_REPEAT = 3,
-HEADER_TRANS_ORIGINATOR_OTHER              = 4,
-HEADER_TRANS_ORIGINATOR_OTHER_REPEAT       = 5,
-
-HEADER_TRANS_ORIGINATOR_RESERVED_ISO_LOW   = 6,
-HEADER_TRANS_ORIGINATOR_RESERVED_ISO_HIGH  = 9
-};
-
-
-int main(int argc, char **argv)
+int ISOmsg_set_field (struct ISOmsg *ptr, int index, char *data, int data_size)
 {
-	printf("%d\n", HEADER_MESSAGE_CLASS_CHRGBAK);
-	return 0;
+	int return_val = 0;
+
+	if (ptr) {
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ * func : make_message
+ * args : void
+ * out  : struct ISOmsg *
+ * use  : get a new ISOmsg ptr
+ */
+
+struct ISOmsg *make_message()
+{
+	struct ISOmsg *ptr = malloc(sizeof(struct ISOmsg));
+
+	/* set the data */
+	ptr->current = 0;
+	ptr->max     = 256;
+	ptr->index = NULL;
+	ptr->data  = NULL;
+
+	return ptr;
+}
+
+/*
+ * func : free_message
+ * args : struct ISOmsg *
+ * out  : void
+ * use  : this will perform a deep free, that is, it will free all data
+ *        utilized within the structure
+ */
+
+void free_message(struct ISOmsg *ptr)
+{
+	if (ptr->data) {
+		free(ptr->data);
+	}
+
+	if (ptr->index) {
+		free(ptr->index);
+	}
+
+	if (ptr) {
+		free(ptr);
+	}
 }
